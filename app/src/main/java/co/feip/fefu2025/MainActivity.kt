c5a05c1e578f307e1bcbca8c04e1b7bfda99eb62
NotTheStas <113777167+NotTheStas@users.noreply.github.com>
2025-03-18 21:55:39 +1000

    animeView

--- app/src/main/java/co/feip/fefu2025/MainActivity.kt
+++ app/src/main/java/co/feip/fefu2025/MainActivity.kt
@@ -1,5 +1,6 @@
 package co.feip.fefu2025
 
+<<<<<<< Updated upstream
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,10 +13,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.feip.fefu2025.ui.theme.FEFU2025AndroidBaseRepoTheme
+=======
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.app.Activity
+
+class MainActivity : Activity() {
+
+    private lateinit var flexBoxLayout: CustomFlexBoxLayout
+>>>>>>> Stashed changes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+<<<<<<< Updated upstream
         enableEdgeToEdge()
         setContent {
             FEFU2025AndroidBaseRepoTheme {
@@ -26,10 +38,21 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+=======
+        setContentView(R.layout.main_activity)
+
+        flexBoxLayout = findViewById(R.id.flexBoxLayout)
+
+        val addButton = findViewById<View>(R.id.addButton)
+
+        addButton.setOnClickListener {
+            flexBoxLayout.addView(createAnimeView())
+>>>>>>> Stashed changes
         }
     }
 }
 
+<<<<<<< Updated upstream
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -43,5 +66,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     FEFU2025AndroidBaseRepoTheme {
         Greeting("Android")
+=======
+    private fun createAnimeView(): AnimeGenreView {
+        val animeView = AnimeGenreView(this)
+
+        val genres = listOf("Комедия", "Повседневность", "Музыка", "Школа", "Дружба", "Фэнтези")
+        val colors = listOf(Color.BLUE, Color.RED, Color.rgb(18, 128, 40), Color.rgb(179, 0, 255), Color.MAGENTA, Color.rgb(255, 111, 0))
+        val randomIndex = (0..5).random()
+
+        animeView.setGenreName(genres[randomIndex])
+        animeView.setBackgroundColor(colors[randomIndex])
+
+        return animeView
+>>>>>>> Stashed changes
     }
 }
