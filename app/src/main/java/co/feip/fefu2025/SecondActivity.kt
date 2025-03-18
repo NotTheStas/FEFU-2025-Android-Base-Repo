8b3a1550ed7b6f85eb918ffd6f49ad68c3b9e69f
NotTheStas <113777167+NotTheStas@users.noreply.github.com>
2025-03-18 17:10:59 +1000

    dz1

---
+++ app/src/main/java/co/feip/fefu2025/SecondActivity.kt
@@ -0,0 +1,13 @@
+package co.feip.fefu2025
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.second_activity)
+    }
+}
